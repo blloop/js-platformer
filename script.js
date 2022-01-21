c415fdc3e0592a68d625954307bda3cdf1973f3d
blloop <42186044+blloop@users.noreply.github.com>
2022-01-20 17:21:05 -0800

    Improved player and platforms

--- script.js
+++ script.js
@@ -7,71 +7,72 @@
     window.requestAnimationFrame = requestAnimationFrame;
 })();
 
+const refresh = document.querySelector('.refresh');
+const refreshPage = () => {
+  location.reload();
+}
+refresh.addEventListener('click', refreshPage);
+
 var canvas = document.getElementById("canvas"), 
     ctx = canvas.getContext("2d"), 
-    width = 1000, 
+    width = 800, 
     height = 500, 
     player = {
         x: 150, 
-        y: height / 2, 
-        width: 20, 
-        height: 20, 
+        y: height - 100, 
+        width: 24, 
+        height: 36, 
         speed: 6, 
         xVel: 0, 
         yVel: 0, 
         jump: false, 
         ground: false,
+        face: 'right',
     }, 
     gravity = 0.5, 
     friction = 0.7, 
     collide = false;
     keys = [], 
     platforms = [];
+    breakable = ['key'];
 
-function createPlatforms() {
-    platforms.push({
-        x: 0, y: 0, color: "black",
-        width: 15, height: height, 
-    });
+function pushPlat1() {
+    platforms.length = 0;
     platforms.push({
         x: 0, y: height - 15, color: "black",
-        width: width, height: 15
+        width: width, height: 15, id: "floor"
     });
     platforms.push({
-        x: 150, y: 140, color: "black",
-        width: 120, height: 15
+        x: 0, y: 0, color: "black",
+        width: 15, height: height, id: "left-wall" 
     });
     platforms.push({
-        x: 150, y: 280, color: "black",
-        width: 120, height: 15
-    });    
-    platforms.push({
-        x: 150, y: 420, color: "black",
-        width: 120, height: 15
+        x: width - 15, y: 300, color: "black",
+        width: 15, height: 200, id: "right-half-wall" 
     });
     platforms.push({
-        x: 380, y: 210, color: "black",
-        width: 120, height: 15
+        x: 480, y: 420, color: "black",
+        width: 100, height: 15, id: "none" 
     });
     platforms.push({
-        x: 380, y: 350, color: "black",
-        width: 120, height: 15
-    });    
-    platforms.push({
-        x: 610, y: 140, color: "black",
-        width: 120, height: 15
+        x: 300, y: 360, color: "black",
+        width: 100, height: 15, id: "none" 
     });
     platforms.push({
-        x: 610, y: 280, color: "black",
-        width: 120, height: 15
+        x: 120, y: 300, color: "black",
+        width: 100, height: 15, id: "none" 
     });
     platforms.push({
-        x: 610, y: 420, color: "black",
-        width: 120, height: 15
+        x: 300, y: 240, color: "black",
+        width: 240, height: 15, id: "none" 
+    });
+    platforms.push({
+        x: 700, y: 300, color: "black",
+        width: 100, height: 15, id: "none" 
     });
 }
 function renderPlatform(input) {
-    ctx.fillStyle = "#5d6d7e";
+    ctx.fillStyle = " #abb2b9 ";
     ctx.rect(
         platforms[input].x, 
         platforms[input].y, 
@@ -79,24 +80,46 @@ function renderPlatform(input) {
         platforms[input].height
     );
 }
+// player shadow: #bfc9ca 
 function renderPlayer() {
-    ctx.fillStyle = "black";
-    ctx.fillRect(
-        player.x, 
-        player.y, 
-        player.width, 
-        player.height
-    );
-    ctx.fillStyle = " #abebc6 ";
-    ctx.fillRect(
-        player.x + 2, 
-        player.y + 2, 
-        player.width - 4, 
-        player.height - 4
-    );
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 1;
+    ctx.fillStyle = "#f7f9f9";
+    ctx.fillRect(player.x + 2, player.y, 20, 16);
+    ctx.fillRect(player.x, player.y + 16, 24, 20);
+    ctx.fillStyle = "#737373";
+    ctx.strokeRect(player.x + 2, player.y, 20, 16);
+    ctx.strokeRect(player.x, player.y + 16, 24, 20); 
+    ctx.fillStyle = "#cc0000";
+    ctx.fillRect(player.x + 2, player.y + 16, 20, 4);
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = "#cc0000";
+    ctx.beginPath();
+    if (player.face == 'right') {
+        ctx.moveTo(player.x + 2, player.y + 16);
+        ctx.lineTo(player.x - 8, player.y + 25);
+    } else {
+        ctx.moveTo(player.x + 22, player.y + 16);
+        ctx.lineTo(player.x + 32, player.y + 25);
+    }
+    ctx.closePath();
+    ctx.stroke();
+    ctx.strokeStyle = "#ff9933";
+    ctx.beginPath();
+    if (player.face == 'right') {
+        ctx.moveTo(player.x + 20, player.y + 7);
+        ctx.lineTo(player.x + 30, player.y + 8);
+        ctx.lineTo(player.x + 20, player.y + 11);
+    } else {
+        ctx.moveTo(player.x + 4, player.y + 7);
+        ctx.lineTo(player.x - 6, player.y + 8);
+        ctx.lineTo(player.x + 4, player.y + 11);
+    }
+    ctx.closePath();
+    ctx.stroke();
 }
 function renderCanvas() {
-    ctx.fillStyle = "#60d6ff"; 
+    ctx.fillStyle = "#d6eaf8 "; 
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 function keyCheck() {
@@ -111,11 +134,13 @@ function keyCheck() {
         if (player.xVel < player.speed) {
             player.xVel = player.speed;
         }
+        player.face = 'right';
     }
     if (keys[37] || keys[65]) {
         if (player.xVel > -player.speed) {
             player.xVel = -player.speed;
         }
+        player.face = 'left';
     }
 }
 
@@ -130,16 +155,24 @@ function engine(){
     player.ground = false;
     for (var i = 0; i < platforms.length; i++) {
         renderPlatform(i);
-        collide = collision(platforms[i]);
-        if (collide == "down") {
-            player.ground = true;
-            player.jump = false;
-        } else if (collide == "up") {
-            player.yVel = 0;
-        } else if (collide == "side") {
-            player.xVel = 0;
-            player.jump = false;
+        var collide = collision(platforms[i]);
+        if (collide && breakable.includes(platforms[i].id)) {
+            if (platforms[i].id == 'key1') {
+
+            }
+            platforms.splice(i, 1);
+        } else {
+            if (collide == "down") {
+                player.ground = true;
+                player.jump = false;
+            } else if (collide == "up") {
+                player.yVel = 0;
+            } else if (collide == "side") {
+                player.xVel = 0;
+                player.jump = false;
+            }
         }
+        
     }   
     if (player.ground) {
         player.yVel = 0;
@@ -180,7 +213,7 @@ function collision(object) {
 }
 canvas.width = width;
 canvas.height = height;
-createPlatforms();
+pushPlat1();
 document.body.addEventListener("keydown", function (input) {
     keys[input.keyCode] = true;
 });
